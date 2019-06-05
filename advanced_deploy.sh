f1ef105a365d5dfaf3a360912854264f6c7e0e44
Eric Nielson <eric.nielson@divvypay.com>
2019-06-05 11:03:37 -0600

    adding taskcat

--- advanced_deploy.sh
+++ advanced_deploy.sh
@@ -1,31 +1,32 @@
 #!/usr/bin/env bash
 stack_name="cft-advanced"
 region="us-west-2"
-package="package-template.yaml"
-bucket="cft-deployments"
+package="templates/package-template.yaml"
+templates_bucket="cft-deployments"
 capabilities="CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND"
 params="NamingPrefix=${stack_name} Email=someone@noreply.com"
 tags="Name=${stack_name}"
 profile="mine"
 
-cd advanced_cft
+taskcat -c ci/taskcat.yml -l -P mine
 
 echo ""
 echo "Building deployment package ${package} for stack ${stack_name}"
-aws cloudformation package --region ${region} --template-file main.yaml --s3-bucket ${bucket} --output-template-file ${package} --profile ${profile}
+aws cloudformation package --region ${region} --template-file templates/main.yaml --s3-bucket ${templates_bucket} --s3-prefix ${stack_name} --output-template-file ${package} --profile ${profile}
+
 echo ""
 echo "Deploying package ${package} for stack ${stack_name}"
-aws cloudformation deploy --region ${region} --template-file ${package} --stack-name ${stack_name} --s3-bucket ${bucket} --s3-prefix ${stack_name} --parameter-overrides ${params} --capabilities ${capabilities} --tags ${tags} --no-execute-changeset --profile ${profile}
+aws cloudformation deploy --region ${region} --template-file ${package} --stack-name ${stack_name} --s3-bucket ${templates_bucket} --s3-prefix ${stack_name} --parameter-overrides ${params} --capabilities ${capabilities} --tags ${tags} --no-execute-changeset --profile ${profile}
 
 rm -rf ${package}
 
 echo ""
 echo "Detecting Drifts.."
-python3 handle_drift.py ${stack_name} ${region} ${profile}
+python3 advanced_cft/handle_drift.py ${stack_name} ${region} ${profile}
 if [[ "$?" != "0" ]]; then
     exit -1
 fi
 
 echo ""
 echo "Executing Change Set.."
-python3 handle_change_set.py ${stack_name} ${region} ${profile}
+python3 advanced_cft/handle_change_set.py ${stack_name} ${region} ${profile}
--- simple_deploy.sh
+++ simple_deploy.sh
@@ -3,21 +3,18 @@ stack_name="cft-simple"
 region="us-west-2"
 deploy_profile="mine"
 
-cf_file="simple_cft/cft.yaml"
-tag_file="simple_cft/tags.json"
-param_file="simple_cft/params.json"
+cd simple_cft
+cf_file="cft.yaml"
+tag_file="tags.json"
+param_file="params.json"
 
 capabilities="CAPABILITY_NAMED_IAM"
 
 aws cloudformation get-template --region ${region} --profile ${deploy_profile} --stack-name ${stack_name} &> /dev/null
 if [[ "$?" != "0" ]]; then
  echo "Stack $stack_name does not currently exist.";
-    aws cloudformation create-stack --profile ${deploy_profile} --region ${region} --stack-name ${stack_name} --template-body file://${cf_file} --tags file://${tag_file} --capabilities ${capabilities} \
---parameters file://${param_file}
-
+    aws cloudformation create-stack --profile ${deploy_profile} --region ${region} --stack-name ${stack_name} --template-body file://${cf_file} --tags file://${tag_file} --capabilities ${capabilities} --parameters file://${param_file}
 else
  echo "Stack $stack_name currently exists.";
-    aws cloudformation update-stack --profile ${deploy_profile} --region ${region} --stack-name ${stack_name} --template-body file://${cf_file} --tags file://${tag_file} --capabilities ${capabilities} \
---parameters file://${param_file}
-
+    aws cloudformation update-stack --profile ${deploy_profile} --region ${region} --stack-name ${stack_name} --template-body file://${cf_file} --tags file://${tag_file} --capabilities ${capabilities} --parameters file://${param_file}
 fi
